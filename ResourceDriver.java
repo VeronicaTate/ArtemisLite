077f74c8918c6dfd28f8752092c0719da71e8868
shawodonnell <58178447+shawodonnell@users.noreply.github.com>
2021-02-23 15:02:40 +0000

    Resource driver example

---
+++ ResourceDriver.java
@@ -0,0 +1,32 @@
+package Management;
+
+
+public class ResourceDriver {
+	
+	
+
+	public static void main(String[] args) throws Exception {
+		
+		/*
+		int total = 300;
+		int human = total/3;
+		int material = total/3;
+		int time = total-(human+material);
+		
+		System.out.println("Human Resource: "+human+ " Material Resource: "+material+" Time Resource: "+ time);
+		*/
+		
+		//-------------------
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+
+}
